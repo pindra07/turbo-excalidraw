f70e3e9a6689e99ccf198850d0d443521911bf03
Priyanshu Indra <indrapriyanshu@gmail.com>
2025-12-24 21:13:22 +0530

    new

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -23,20 +23,20 @@ app.post("/signup", async (req, res) => {
     });
   }
 
-  // try {
-  //   //@ts-ignore
-  //   await prisma.User.create({
-  //     data: {
-  //       email: parsedData.data?.username,
-  //       password: parsedData.data.password,
-  //       name: parsedData.data.username,
-  //     },
-  //   });
-  // } catch (e) {
-  //   res.status(411).json({
-  //     message: "User already exists...",
-  //   });
-  // }
+  try {
+    //@ts-ignore
+    await prisma.user.create({
+      data: {
+        email: parsedData.data?.username,
+        password: parsedData.data.password,
+        name: parsedData.data.username,
+      },
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists...",
+    });
+  }
 
   // user.findOne({username, password})
   // if found user... user already exist...
